918f75c56650914154e4712b4621a81cbf9c7f7c
Kevin D <42454194+Kjay1181@users.noreply.github.com>
2019-04-10 21:27:39 -1000

    Add files via upload

--- cheese.js
+++ cheese.js
@@ -1,10 +1,11 @@
 function cheese(){
-  this.x=50;
+  this.x=Math.floor(Math.random()*w);
   this.y=50;
   this.width=100;
   this.height=100;
+  this.count=0;
   this.show=function(){
     fill(color("green"));
-    rect(this.x,this.y,this.width,this.height);
+    ellipse(this.x,this.y,this.width,this.height);
   }
 }
